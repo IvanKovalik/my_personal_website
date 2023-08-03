3597444ebb61afe7a96544a2bd65924d68b90890
IvanKovalik <qwezx004@yandex.ru>
2023-08-03 21:10:40 +0700

    Working on Django, filtering by tags

--- my_personal_website/views.py
+++ my_personal_website/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from .models import Tag, Article
 
 
 class IndexPageView(TemplateView):
@@ -16,6 +19,11 @@ class ProjectsPageView(TemplateView):
 
 class ArticlesPageView(TemplateView):
     template_name = 'articles_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
 
 
 class ContactPageView(TemplateView):
